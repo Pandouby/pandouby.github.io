fb5a54972446574d27b0d5a88bfa4d18858cb40c
Pandouby <57749377+Pandouby@users.noreply.github.com>
2023-05-19 23:34:57 +0200

    Project title color fix

--- src/layout/ProjectSection/ProjectSection.tsx
+++ src/layout/ProjectSection/ProjectSection.tsx
@@ -1,16 +1,20 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import "./styles.scss";
-import Card from "../Card/Card";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const ProjectSection: FC = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+      target: ref,
+      offset: ["end end", "center center"],
+  });
+
+  const font = useTransform(scrollYProgress, [0.2, 1], [1, 0.45]);
+  const fontSize = useTransform(font, (s) => `scale3d(${s},${s},1)`);
+
   return (
     <div className="section project-section">
-      <div className="project-title">projects</div>
-      <div className="projects-wrapper">
-        <Card title="Game"> 
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est, facere distinctio! Velit minima voluptatem veniam magnam ex culpa laboriosam labore voluptatum optio incidunt quas, quam a beatae consequuntur, dolores ipsa.</p>
-        </Card>
-      </div>
+      <motion.div className="project-title" ref={ref} style={{transform: fontSize}}>projects</motion.div>
     </div>
   );
 };
