7fc90081cb5f36c19181baeaf9b9439dcec66a20
Pandouby <57749377+Pandouby@users.noreply.github.com>
2025-07-29 01:13:08 +0200

    Add Image viewer for gallery and layout animation for opening images

--- src/components/GallerySection/GalleryImage.tsx
+++ src/components/GallerySection/GalleryImage.tsx
@@ -1,5 +1,10 @@
-import { motion, useScroll, useTransform } from "framer-motion";
-import React, { FC, useRef } from "react";
+import {
+	AnimatePresence,
+	motion,
+	useScroll,
+	useTransform,
+} from "framer-motion";
+import React, { FC, useEffect, useRef, useState } from "react";
 
 export enum GalleryImageOrientation {
 	PORTRAIT = "portrait",
@@ -8,21 +13,23 @@ export enum GalleryImageOrientation {
 }
 
 export interface GalleryImageProps {
-	src: string;
+	highResSrc: string;
+	lowResSrc: string;
 	alt: string;
 	orientation: GalleryImageOrientation;
 	offset: { offsetX: number; offsetY: number };
 	scrollMultiplier?: number;
-    style?: React.CSSProperties;
+	style?: React.CSSProperties;
 }
 
 export const GalleryImage: FC<GalleryImageProps> = ({
-	src,
+	highResSrc,
+	lowResSrc,
 	alt,
 	orientation,
 	offset,
 	scrollMultiplier = 0,
-    style
+	style,
 }) => {
 	const ref = useRef(null);
 	const { scrollYProgress } = useScroll({
@@ -30,27 +37,62 @@ export const GalleryImage: FC<GalleryImageProps> = ({
 		offset: ["start end", "end start"],
 	});
 
-	const translatePos = useTransform(scrollYProgress, [0, 1], [0, 100]);
-	const translateYPos = () =>
-		useTransform(
-			translatePos,
-			(x) =>
-				`translate(${offset.offsetX}px, ${
-					offset.offsetY - x * scrollMultiplier
-				}px)`
-		);
+	const [isImageViewerOpen, setIsImageViewerOpen] = useState(false);
 
-	const transform = {
-		transform: translateYPos(),
+	const handleImageClick = () => {
+		setIsImageViewerOpen(!isImageViewerOpen);
 	};
 
+	useEffect(() => {
+		if (isImageViewerOpen) {
+			document.body.style.overflow = "hidden";
+			document.body.style.height = "100vh";
+		} else {
+			document.body.style.overflow = "auto";
+			document.body.style.height = "100%";
+		}
+	}, [isImageViewerOpen]);
+
+	const translate = useTransform(scrollYProgress, [0, 1], [0, 100]);
+	const translateY = useTransform(translate, (x) => {
+		return `translate(${offset.offsetX}px, ${
+			offset.offsetY - x * scrollMultiplier
+		}px)`;
+	});
+
+	const transformStyle = !isImageViewerOpen
+		? {
+				transform: translateY,
+		  }
+		: {};
+
 	return (
-		<motion.img
-			src={src}
-			className={orientation}
-			alt={alt}
-			style={{...style, ...transform}}
-			ref={ref}
-		/>
+		<>
+			<motion.div className="gallery-image-wrapper" style={{ ...style }} layoutId={`wrapper-${lowResSrc}`}>
+				<motion.img
+					src={lowResSrc}
+					className={orientation}
+					layoutId={alt}
+					alt={alt}
+					//style={{...transformStyle}}
+					//ref={ref}
+					onClick={handleImageClick}
+				/>
+			</motion.div>
+
+			{isImageViewerOpen && (
+				<div className="image-viewer">
+					<motion.div className="gallery-image-wrapper" layoutId={`wrapper-${lowResSrc}`}>
+						<motion.img
+							src={lowResSrc}
+							className={orientation}
+							layoutId={alt}
+							onClick={handleImageClick}
+						/>
+                        <p>Wonderfull holliday</p>
+					</motion.div>
+				</div>
+			)}
+		</>
 	);
 };
--- src/components/GallerySection/GallerySection.tsx
+++ src/components/GallerySection/GallerySection.tsx
@@ -11,109 +11,133 @@ import {
 export const GallerySection: FC = () => {
 	const imageList: GalleryImageProps[] = [
 		{
-            src: "/images/Gallery/1.jpeg",
-            alt: "Image 1",
-            orientation: GalleryImageOrientation.PORTRAIT,
-            offset: { offsetX: 63, offsetY: -43 },
-            scrollMultiplier: 2,
-        },
+			highResSrc: "/images/Gallery/high-res/image1.JPG",
+			lowResSrc: "/images/Gallery/low-res/image1.JPG",
+			alt: "Image 1",
+			orientation: GalleryImageOrientation.PORTRAIT,
+			offset: { offsetX: 63, offsetY: -43 },
+			scrollMultiplier: 2,
+		},
 		{
-			src: "/images/Gallery/2.jpeg",
+			highResSrc: "/images/Gallery/high-res/image2.jpg",
+			lowResSrc: "/images/Gallery/low-res/image2.JPG",
 			alt: "Image 2",
 			orientation: GalleryImageOrientation.PORTRAIT,
 			offset: { offsetX: -52, offsetY: -78 },
 			scrollMultiplier: 0.5,
 		},
 		{
-			src: "/images/Gallery/3.jpeg",
+			highResSrc: "/images/Gallery/high-res/image3.jpg",
+			lowResSrc: "/images/Gallery/low-res/image3.jpg",
 			alt: "Image 3",
 			orientation: GalleryImageOrientation.PORTRAIT,
 			offset: { offsetX: -31, offsetY: 47 },
 		},
 		{
-			src: "/images/Gallery/4.jpeg",
+			highResSrc: "/images/Gallery/high-res/image4.JPG",
+			lowResSrc: "/images/Gallery/low-res/image4.JPG",
 			alt: "Image 4",
-			orientation: GalleryImageOrientation.PORTRAIT,
+			orientation: GalleryImageOrientation.LANDSCAPE,
 			offset: { offsetX: -66, offsetY: 6 },
 			scrollMultiplier: 2,
 		},
 		{
-			src: "/images/Gallery/5.jpeg",
+			highResSrc: "/images/Gallery/high-res/image5.JPG",
+			lowResSrc: "/images/Gallery/low-res/image5.JPG",
 			alt: "Image 5",
-			orientation: GalleryImageOrientation.LANDSCAPE,
+			orientation: GalleryImageOrientation.PORTRAIT,
 			offset: { offsetX: 92, offsetY: -48 },
 		},
 		{
-			src: "/images/Gallery/6.jpeg",
+			highResSrc: "/images/Gallery/high-res/image6.JPG",
+			lowResSrc: "/images/Gallery/low-res/image6.JPG",
 			alt: "Image 6",
 			orientation: GalleryImageOrientation.LANDSCAPE,
 			offset: { offsetX: 59, offsetY: 70 },
 		},
 		{
-			src: "/images/Gallery/7.jpeg",
+			highResSrc: "/images/Gallery/high-res/image7.JPG",
+			lowResSrc: "/images/Gallery/low-res/image7.JPG",
 			alt: "Image 7",
 			orientation: GalleryImageOrientation.PORTRAIT,
 			offset: { offsetX: -15, offsetY: 65 },
 			scrollMultiplier: 2.5,
 		},
 		{
-			src: "/images/Gallery/8.jpeg",
+			highResSrc: "/images/Gallery/high-res/image8.JPG",
+			lowResSrc: "/images/Gallery/low-res/image8.JPG",
 			alt: "Image 8",
-			orientation: GalleryImageOrientation.PORTRAIT,
+			orientation: GalleryImageOrientation.LANDSCAPE,
 			offset: { offsetX: -44, offsetY: -3 },
 			scrollMultiplier: 1.4,
 		},
 		{
-			src: "/images/Gallery/9.jpeg",
+			highResSrc: "/images/Gallery/high-res/image9.JPG",
+			lowResSrc: "/images/Gallery/low-res/image9.JPG",
 			alt: "Image 9",
 			orientation: GalleryImageOrientation.PORTRAIT,
 			offset: { offsetX: 14, offsetY: -19 },
 			scrollMultiplier: 0.3,
 		},
 		{
-			src: "/images/Gallery/10.jpeg",
+			highResSrc: "/images/Gallery/high-res/image10.JPG",
+			lowResSrc: "/images/Gallery/low-res/image10.JPG",
 			alt: "Image 10",
 			orientation: GalleryImageOrientation.LANDSCAPE,
 			offset: { offsetX: 68, offsetY: -80 },
 			scrollMultiplier: 1.7,
 		},
 		{
-			src: "/images/Gallery/11.jpeg",
+			highResSrc: "/images/Gallery/high-res/image11.JPG",
+			lowResSrc: "/images/Gallery/low-res/image11.JPG",
 			alt: "Image 11",
 			orientation: GalleryImageOrientation.PORTRAIT,
 			offset: { offsetX: -55, offsetY: 34 },
 		},
 		{
-			src: "/images/Gallery/12.jpeg",
+			highResSrc: "/images/Gallery/high-res/image12.JPG",
+			lowResSrc: "/images/Gallery/low-res/image12.JPG",
 			alt: "Image 12",
 			orientation: GalleryImageOrientation.LANDSCAPE,
 			offset: { offsetX: -72, offsetY: 25 },
 		},
 		{
-			src: "/images/Gallery/13.jpeg",
+			highResSrc: "/images/Gallery/high-res/image13.JPG",
+			lowResSrc: "/images/Gallery/low-res/image13.JPG",
 			alt: "Image 13",
-			orientation: GalleryImageOrientation.LANDSCAPE,
+			orientation: GalleryImageOrientation.PORTRAIT,
 			offset: { offsetX: 33, offsetY: -54 },
 			scrollMultiplier: 1.1,
 		},
 		{
-			src: "/images/Gallery/14.jpeg",
+			highResSrc: "/images/Gallery/high-res/image14.JPG",
+			lowResSrc: "/images/Gallery/low-res/image14.JPG",
 			alt: "Image 14",
 			orientation: GalleryImageOrientation.PORTRAIT,
 			offset: { offsetX: -5, offsetY: 38 },
 			scrollMultiplier: 1.3,
 		},
 		{
-			src: "/images/Gallery/15.jpeg",
+			highResSrc: "/images/Gallery/high-res/image15.JPG",
+			lowResSrc: "/images/Gallery/low-res/image15.JPG",
 			alt: "Image 15",
 			orientation: GalleryImageOrientation.PORTRAIT,
 			offset: { offsetX: 3, offsetY: -4 },
 			scrollMultiplier: 0.6,
 		},
 		{
-			src: "/images/Gallery/16.jpeg",
+			highResSrc: "/images/Gallery/high-res/image16.JPG",
+			lowResSrc: "/images/Gallery/low-res/image16.JPG",
 			alt: "Image 16",
 			orientation: GalleryImageOrientation.PORTRAIT,
+			offset: { offsetX: -47, offsetY: -27 },
+			scrollMultiplier: 0.2,
+		},
+        {
+			highResSrc: "/images/Gallery/high-res/image17.JPG",
+			lowResSrc: "/images/Gallery/low-res/image17.JPG",
+			alt: "Image 17",
+			orientation: GalleryImageOrientation.LANDSCAPE,
 			offset: { offsetX: -47, offsetY: -27 },
 			scrollMultiplier: 0.2,
 		},
@@ -127,12 +151,13 @@ export const GallerySection: FC = () => {
 				{imageList.map((image, index) => (
 					<GalleryImage
 						key={index}
-						src={image.src}
+                        highResSrc={image.highResSrc}
+						lowResSrc={image.lowResSrc}
 						alt={image.alt}
 						orientation={image.orientation}
 						offset={image.offset}
 						scrollMultiplier={image.scrollMultiplier} // Random multiplier for scroll effect
-                        style={{ gridArea: "box-" + (index + 1) }}
+						style={{ gridArea: "box-" + (index + 1) }}
 					/>
 				))}
 			</div>
